33c3de264ade983d8d4e21fbb612a40f8be43dcb
RobertoZUpqroo <202200553@upqroo.edu.mx>
2023-11-30 21:17:00 -0500

    Footer casi terminado login agrega|do
    si

---
+++ Migrations/20231201004226_ScireHubDb.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScireHub.Migrations
+{
+    /// <inheritdoc />
+    public partial class ScireHubDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    PkRoles = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.PkRoles);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    PKUsuario = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellido1 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellido2 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NombreUsuario = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Correo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Contraseña = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FkRol = table.Column<int>(type: "int", nullable: true),
+                    UrlImagenPath = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.PKUsuario);
+                    table.ForeignKey(
+                        name: "FK_Usuarios_Roles_FkRol",
+                        column: x => x.FkRol,
+                        principalTable: "Roles",
+                        principalColumn: "PkRoles");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Investigaciones",
+                columns: table => new
+                {
+                    PkInvestigación = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Categoría = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Fecha = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FkAutor = table.Column<int>(type: "int", nullable: true),
+                    UrlPdfPath = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Investigaciones", x => x.PkInvestigación);
+                    table.ForeignKey(
+                        name: "FK_Investigaciones_Usuarios_FkAutor",
+                        column: x => x.FkAutor,
+                        principalTable: "Usuarios",
+                        principalColumn: "PKUsuario");
+                });
+
+            migrationBuilder.InsertData(
+                table: "Usuarios",
+                columns: new[] { "PKUsuario", "Apellido1", "Apellido2", "Contraseña", "Correo", "FkRol", "Nombre", "NombreUsuario", "UrlImagenPath" },
+                values: new object[] { 1, "Fierro", "Ballote", "1234", "robertojunio2004@gmail.com", null, "Roberto", "robertofb", null });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Investigaciones_FkAutor",
+                table: "Investigaciones",
+                column: "FkAutor");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuarios_FkRol",
+                table: "Usuarios",
+                column: "FkRol");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Investigaciones");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+        }
+    }
+}
